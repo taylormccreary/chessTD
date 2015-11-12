bc0f86ed6f32c6f9ad4f0e0e90e182fd87ac2eaf
Taylor McCreary <taylor@mccreary.cc>
2015-11-12 13:23:01 -0800

    It works now

--- displayPairings.js
+++ displayPairings.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
 })
 
-function zdisplayInitialPairings(round, sec) {
+function displayInitialPairings(round, sec) {
     firstRound = false;
     var i;
     for (i = 0; i < round.length; i++) {
